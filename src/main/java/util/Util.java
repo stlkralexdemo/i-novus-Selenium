5bb56d9509d01174ad62ef47e6d409c36c0cb56d
stlkralexdemo <stlkralex@yandex.ru>
2019-03-21 12:45:13 +0300

    Добавил комментарии, описывающие некоторые действия в формате JavaDoc. Форматировал код.

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -2,24 +2,29 @@ package util;
 
 import java.util.*;
 
+
 public class Util {
+    /**
+     * Метод для вычисления рандомного числа от 0 и до количества товаров на странице пластинок
+     */
     public static int rand(int max) {
+
         Random random = new Random();
-        int i = random.nextInt(max - 1);
-        return i;
+        return random.nextInt(max - 1);
     }
 
+    /**
+     * Метод для перевода цены из строки в число, с удалением лишних пробелов, если имеются
+     */
     public static int parseNum(String price) {
+
         String[] numbers = price.split("");
         String numberNew = "";
         for (String number : numbers) {
-            if (number.equals(" ")){
-
-            } else numberNew += number;
+            if (!number.equals(" ")) {
+                numberNew += number;
+            }
         }
-
         return Integer.parseInt(numberNew);
-
-
     }
 }
--- src/test/java/ru/ozon/Test.java
+++ src/test/java/ru/ozon/Test.java
@@ -25,6 +25,7 @@ public class Test {
     @org.testng.annotations.Test(priority = 1, description = "Открыть в браузере сайт https://www.ozon.ru/. Если откроется всплывающее окно – закрыть\n" +
             "его.")
     public void first() {
+
         System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\drivers\\geckodriver.exe");
 
         driver = new FirefoxDriver();
@@ -36,7 +37,6 @@ public class Test {
 
         driver.findElementByCssSelector(".catalog-button-text").click();
         driver.findElementByCssSelector("a.top-level-section:nth-child(19) > span:nth-child(1)").click();
-
     }
 
     @org.testng.annotations.Test(priority = 3, description = "С открывшейся страницы перейти на страницу \"Виниловые пластинки\".")
@@ -45,6 +45,9 @@ public class Test {
         driver.findElementByCssSelector("div.products-carousel:nth-child(3) > a:nth-child(1)").click();
     }
 
+    /**
+     * Если на странице много элементов с атрибутами index и id - значит открылся именно список товаров. Конкретно в этом случае, если их больше 20. Число может быть и другим.
+     */
     @org.testng.annotations.Test(priority = 4, description = "Проверить, что открылся список товаров.")
     public void truePage() {
         List<WebElement> elements = driver.findElementsByXPath("//div[@class = 'tile' and @index and @id]");
@@ -52,6 +55,9 @@ public class Test {
         Assert.assertTrue(elements.size() > 20);
     }
 
+    /**
+     * Считаем количество элементов в списке с атрибутами index и id. Полученное число присваиваем переменной класса amount, оно ещё пригодится.
+     */
     @org.testng.annotations.Test(priority = 5, description = "Получить количество товаров на странице.")
     public void totalAmount() {
 
@@ -60,8 +66,12 @@ public class Test {
         System.out.println("количество товара на странице " + amount);
     }
 
+    /**
+     * Вычисляем рандомное число с помощью метода из Util класса.
+     */
     @org.testng.annotations.Test(priority = 6, description = "Сгенерировать случайное число в диапазоне от 1 до количества товаров, полученного в п.5")
     public void rnd() {
+
         firstRandomNumber = Util.rand(amount);
     }
 
@@ -75,8 +85,12 @@ public class Test {
         driver.findElementByCssSelector(".close-icon").click();
     }
 
+    /**
+     * Получаем цену и наименование товара в строковом виде, сохраняем их в переменных класса firstName и firstPrice. Вывод в консоль не обязателен , нужен мне для визуального контроля.
+     */
     @org.testng.annotations.Test(priority = 8, description = "Запомнить стоимость и название данного товара.")
     public void save() {
+
         firstName = driver.findElementByXPath("//div[@class = 'bDetailPanel']/h1[text()]").getText();
         firstPrice = driver.findElementByXPath("//span[@class = 'eOzonPrice_main']").getText();
 
@@ -86,39 +100,55 @@ public class Test {
 
     @org.testng.annotations.Test(priority = 9, description = "Добавить товар в корзину.")
     public void inBasket() {
+
         driver.findElementByCssSelector(".bSaleBlockButton").click();
     }
 
+    /**
+     * Сравниваем имя товара из корзины с сохраненным ранее в переменной класса firstName.
+     */
     @org.testng.annotations.Test(priority = 10, description = "Проверить то, что в корзине появился добавленный в п.9 товар. ( Проверка данных\n" +
             "определенного товара. Необходим переход в корзину для этого. )")
     public void firstCheckBasket() {
+
         driver.findElementByCssSelector("a.bSaleBlockButton").click();
         String checkName = driver.findElementByCssSelector(".title").getText();
         System.out.println("this first" + checkName);
         Assert.assertEquals(checkName, firstName);
     }
-
+    /**
+     * Дважды жмём кнопку назад в браузере.
+     */
     @org.testng.annotations.Test(priority = 11, description = "Вернуться на страницу \"Виниловые пластинки\".")
     public void back() {
-        driver.navigate().back();
-        driver.navigate().back();
 
+        driver.navigate().back();
+        driver.navigate().back();
     }
 
+    /**
+     * Вычисляем новое рандомное число с помощью метода из Util класса.
+     */
     @org.testng.annotations.Test(priority = 12, description = "Сгенерировать случайное число в диапазоне от 1 до количества товаров, полученного в\n" +
             "п.5")
     public void secondRnd() {
+
         secondRandomNumber = Util.rand(amount);
     }
 
     @org.testng.annotations.Test(priority = 13, description = "Выбрать товар под номером, полученным в п.12. ( Перейти на страницу товара )")
     public void randomClickNext() {
+
         String xpath = "//div[@index = '" + secondRandomNumber + "']/a";
         driver.findElementByXPath(xpath).click();
     }
 
+    /**
+     * Получаем цену и наименование товара в строковом виде, сохраняем их в переменных класса secondPrice и secondName.
+     */
     @org.testng.annotations.Test(priority = 14, description = "Запомнить стоимость и название данного товара.")
     public void saveSecond() {
+
         secondName = driver.findElementByXPath("//div[@class = 'bDetailPanel']/h1[text()]").getText();
         secondPrice = driver.findElementByXPath("//span[@class = 'eOzonPrice_main']").getText();
 
@@ -128,10 +158,13 @@ public class Test {
 
     @org.testng.annotations.Test(priority = 15, description = "Добавить товар в корзину.")
     public void inBasketSecond() {
-        driver.findElementByCssSelector(".bSaleBlockButton").click();
 
+        driver.findElementByCssSelector(".bSaleBlockButton").click();
     }
 
+    /**
+     * Проверяем значение текстового поля с атрибутом class = 'count', не открывая корзину. Сравниваем это число с числом добавленных товаров в корзину.
+     */
     @org.testng.annotations.Test(priority = 16, description = "Проверить то, что в корзине два товара. ( Проверка количества товаров в корзине. Может\n" +
             "быть произведена без открытия корзины, а проверяя значение в header сайта, где указано\n" +
             "количество товаров в корзине )")
@@ -139,19 +172,23 @@ public class Test {
 
         driver.navigate().back();
         String attribute = driver.findElementByCssSelector(".count").getText();
-        System.out.println(attribute);
 
         Assert.assertEquals(attribute, "2");
     }
 
     @org.testng.annotations.Test(priority = 17, description = "Открыть корзину.")
     public void toBasket() {
+
         driver.get("https://www.ozon.ru/context/cart");
     }
 
+    /**
+     * Проверяем текстовые значения именоавний товаров в корзине с ранее сохраненными в переменных класса. Для вычисления общей стоимости используем метод класса Util. Сравниваем вычисленную стоимость со значением из корзины.
+     */
     @org.testng.annotations.Test(priority = 18, description = "Проверить то, что в корзине раннее выбранные товары и итоговая стоимость по двум\n" +
             "товарам рассчитана верно.")
     public void checkBasket() {
+
         List<WebElement> elements = driver.findElementsByXPath("//div/a[@data-test-id = 'cart-item-title']");
         List<String> list = new ArrayList<String>();
         for (WebElement element : elements) {
@@ -167,12 +204,17 @@ public class Test {
 
     @org.testng.annotations.Test(priority = 19, description = "Удалить из корзины все товары.")
     public void clearBasket() {
+
         driver.findElementByCssSelector("button.header-btn:nth-child(2)").click();
         driver.findElementByCssSelector(".blue").click();
     }
 
+    /**
+     * Ищем на странице текст - Корзина пуста.
+     */
     @org.testng.annotations.Test(priority = 20, description = "Проверить, что корзина пуста.")
     public void nextCheckBasket() {
+
         String text = driver.findElementByXPath("//div[@class = 'empty-cart']/h1[text()]").getText();
         Assert.assertEquals(text, "Корзина пуста");
     }
@@ -180,6 +222,7 @@ public class Test {
 
     @org.testng.annotations.Test(priority = 21, description = "Закрыть браузер.")
     public void afterAll() {
+
         driver.quit();
     }
 }
